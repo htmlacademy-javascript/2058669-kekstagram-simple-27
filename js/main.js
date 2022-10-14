c42fc465c0b5fdb50926c51012d10976ba6c5445
CorisMorri <103724309+CorisMorri@users.noreply.github.com>
2022-10-14 16:53:36 +0300

    Больше деталей, правки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-function getRandomPositiveInteger (a, b) {
+const MIN_ID = 1;
+
+const MAX_ID = 25;
+
+const MIN_LIKES = 15;
+
+const MAX_LIKES = 200;
+
+const MIN_COMMENTS = 0;
+
+const MAX_COMMENTS = 200;
+
+const SIMILAR_PHOTO_COUNT = 25;
+
+const getRandomPositiveInteger = (a, b) => {
 
   if (a < 0 || b < 0) {
     return NaN;
@@ -10,16 +24,14 @@ function getRandomPositiveInteger (a, b) {
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
+const checkStringLength = (string, length) => string.length <= length;
+checkStringLength('string', 4);
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-
-function createRandomIdFromRangeGenerator (min, max) {
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
@@ -33,33 +45,29 @@ function createRandomIdFromRangeGenerator (min, max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
+};
 
-const GENERATE_ID = createRandomIdFromRangeGenerator(1, 25);
+const generateId = createRandomIdFromRangeGenerator(MIN_ID, MAX_ID);
 
-const GENERATE_URL = createRandomIdFromRangeGenerator(1, 25);
+const generateLikesPhoto = createRandomIdFromRangeGenerator(MIN_LIKES, MAX_LIKES);
 
-const DESCRIPTION_PHOTO = ['qeks', 'weks', 'eeks', 'reks', 'teks',
-  'yeks', 'ueks', 'ieks', 'oeks', 'peks',
-  'aeks', 'seks', 'deks', 'feks', 'geks',
-  'heks', 'jeks', 'keks', 'leks', 'zeks',
-  'xeks', 'ceks', 'veks', 'beks', 'neks',];
+const generateCommentsPhoto = createRandomIdFromRangeGenerator(MIN_COMMENTS, MAX_COMMENTS);
 
-const LIKES_PHOTO = Array.from({length: 186}, (_, i) => i + 15);
+const descriptionPhoto = ['Любовь состоит из деталей, и одна из них - это поддержка.', 'Koгдa вы гoвopитe друг другу кoмплимeнты.',
+  'Потому что кaждoмy нyжeн дpyг.', 'Heт никoгo суровее чем лyчшиe дpyзья',
+  'Koгдa ты нeмнoгo стеснительный', 'Когнитивный диccoнaнc', 'Heмнoгo poмaнтики вам в лeнтy.'];
 
-const COMMENTS_PHOTO = Array.from(Array(201).keys());
+const createDescription = function() {
+  const idPhoto = generateId();
+  return {
+    id: idPhoto,
+    url: `photos/${idPhoto}.jpg`,
+    description: getRandomArrayElement(descriptionPhoto),
+    likes: generateLikesPhoto(),
+    comments: generateCommentsPhoto()
+  };
+};
 
-const SIMILAR_PHOTO_COUNT = 25;
-
-
-const createDescription = () => ({
-  id: GENERATE_ID(),
-  url: `photos/${GENERATE_URL()}.jpg`,
-  description: getRandomArrayElement(DESCRIPTION_PHOTO),
-  likes: getRandomArrayElement(LIKES_PHOTO),
-  comments: getRandomArrayElement(COMMENTS_PHOTO)
-});
-
+// eslint-disable-next-line no-unused-vars
 const similarDescriptions = Array.from({length: SIMILAR_PHOTO_COUNT}, createDescription);
 
-console.log(similarDescriptions);
