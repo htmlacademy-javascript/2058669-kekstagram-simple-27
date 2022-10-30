68cffd53f7ded1b8b85dc9d13feb48d74118c6ac
CorisMorri <103724309+CorisMorri@users.noreply.github.com>
2022-10-30 23:11:15 +0300

    Отрисуй меня полностью (часть 1) правки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line no-unused-vars
+
 import{similarDescriptions} from './data.js';
-// eslint-disable-next-line no-unused-vars
+
 import{addPictures} from './picture.js';
+
+addPictures(similarDescriptions);
